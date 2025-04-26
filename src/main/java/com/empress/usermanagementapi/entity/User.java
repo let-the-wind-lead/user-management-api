9e730caa99151285bd088fec583be63fcd8172d3
let-the-wind-lead <yrawenariel8@gmail.com>
2025-04-26 20:36:21 +0200

    Add User entity

---
+++ src/main/java/com/empress/usermanagementapi/entity/User.java
@@ -0,0 +1,19 @@
+package com.empress.usermanagementapi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+    // Getters and setters will be added later
+}
