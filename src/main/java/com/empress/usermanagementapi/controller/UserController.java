66899b04c3fd5f608cdf8d1436e2a48f1d095d6f
let-the-wind-lead <yrawenariel8@gmail.com>
2025-04-26 22:01:59 +0200

    Fix imports for CRUD endpoints

--- src/main/java/com/empress/usermanagementapi/controller/UserController.java
+++ src/main/java/com/empress/usermanagementapi/controller/UserController.java
@@ -1,4 +1,8 @@
 package com.empress.usermanagementapi.controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,9 +10,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, Empress!";
-    }
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, Empress!";
+    	}
+
+	@PostMapping("/users")
+	public String createUser() {
+    		return "User created!";
+	}
+
+	@GetMapping("/users")
+	public String getAllUsers() {
+	    return "List of users!";
+	}
+
+	@PutMapping("/users/{id}")
+	public String updateUser(@PathVariable Long id) {
+    		return "User with ID " + id + " updated!";
+	}
+
+	@DeleteMapping("/users/{id}")
+	public String deleteUser(@PathVariable Long id) {
+    		return "User with ID " + id + " deleted!";
+	}
+
 }
 
