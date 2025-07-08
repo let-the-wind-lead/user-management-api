c4063336263243f8efc07bb0a4fb32259ed3c039
let-the-wind-lead <99548776+let-the-wind-lead@users.noreply.github.com>
2025-07-09 00:15:30 +0200

    Update PageController.java

--- src/main/java/com/empress/usermanagementapi/config/PageController.java
+++ src/main/java/com/empress/usermanagementapi/config/PageController.java
@@ -6,18 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-  @GetMapping("/login")
-  public String login() {
-    return "login";   // src/main/resources/templates/login.html
-  }
+    @GetMapping("/login")
+    public String login() {
+        return "login";   // resolves to login.html
+    }
 
-  @GetMapping("/admin")
-  public String admin() {
-    return "admin";   // src/main/resources/templates/admin.html
-  }
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";   // resolves to admin.html
+    }
 
-  @GetMapping("/user")
-  public String user() {
-    return "user";    // src/main/resources/templates/user.html
-  }
+    @GetMapping("/user")
+    public String user() {
+        return "user";    // resolves to user.html
+    }
 }
