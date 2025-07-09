b60c9ae381181f1f6aef268864c869bc256c6ca0
let-the-wind-lead <99548776+let-the-wind-lead@users.noreply.github.com>
2025-07-09 18:03:57 +0200

    Update PasswordResetTokenRepository.java

--- src/main/java/com/empress/usermanagementapi/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/empress/usermanagementapi/repository/PasswordResetTokenRepository.java
@@ -2,7 +2,6 @@ package com.empress.usermanagementapi.repository;
 
 import com.empress.usermanagementapi.entity.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
