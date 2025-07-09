4d07896874707c67a5c148c4a86b4a0f682071a6
let-the-wind-lead <99548776+let-the-wind-lead@users.noreply.github.com>
2025-07-09 18:11:41 +0200

    Update UserManagementApiApplication.java

--- src/main/java/com/empress/usermanagementapi/UserManagementApiApplication.java
+++ src/main/java/com/empress/usermanagementapi/UserManagementApiApplication.java
@@ -2,12 +2,12 @@ package com.empress.usermanagementapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.empress.usermanagementapi.repository")
 @SpringBootApplication
 public class UserManagementApiApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(UserManagementApiApplication.class, args);
     }
+
 }
