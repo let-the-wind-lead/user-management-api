8d3ee606449bb80e5f70f1f33042fef2bed73d4e
let-the-wind-lead <99548776+let-the-wind-lead@users.noreply.github.com>
2025-07-09 18:03:39 +0200

    Update UserRepository.java

--- src/main/java/com/empress/usermanagementapi/repository/UserRepository.java
+++ src/main/java/com/empress/usermanagementapi/repository/UserRepository.java
@@ -2,10 +2,9 @@ package com.empress.usermanagementapi.repository;
 
 import com.empress.usermanagementapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
-    Optional<User> findByEmail(String email);   // ← new
+    Optional<User> findByEmail(String email);
 }
