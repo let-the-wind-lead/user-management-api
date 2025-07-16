f7c27ff8537328033bcf8b2bcb63406089ee1c77
let-the-wind-lead <yrawenariel8@gmail.com>
2025-07-17 01:02:14 +0200

    repo: add existsByUsername helper

--- src/main/java/com/empress/usermanagementapi/repository/UserRepository.java
+++ src/main/java/com/empress/usermanagementapi/repository/UserRepository.java
@@ -9,4 +9,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsernameAndEmail(String username, String email);
     User findByUsername(String username);
     boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 }
